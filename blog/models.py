bd7d3f505613a1d14962946d6caa64c585907ad6
Yara-SalahElDine <yarasalah94@hotmail.com>
2018-02-13 16:11:34 +0200

    edit models.py

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from django.db import models
 
 
-class User(models.Model):
+class Users(models.Model):
 	user_name = models.CharField(max_length = 50)
 	email=models.EmailField(max_length = 60)
 	password=models.CharField(max_length = 10)
@@ -11,23 +11,16 @@ class User(models.Model):
 	is_blocked=models.BooleanField()
 	def __str__(self):
 		return self.user_name
-	def __str__(self):
-		return self.email
-	def __str__(self):
-		return self.created_at
-	def __str__(self):
-		return self.is_admin
-	def __str__(self):
-		return self.is_blocked
 
 
 
 
-class Category(models.Model):
-	cat_name=models.CharField(max_length = 50)
+
+class Categories(models.Model):
+	category_name=models.CharField(max_length = 50)
 	user = models.ManyToManyField(User)
 	def __str__(self):
-		return self.cat_name
+		return self.category_name
 
 class Posts(models.Model):
 	title=models.CharField(max_length = 50)
@@ -35,65 +28,40 @@ class Posts(models.Model):
 	picture=models.ImageField()
 	content=models.CharField(max_length = 200)
 	user = models.ForeignKey(User)
-	cat=models.ForeignKey(Category)
+	category=models.ForeignKey(Category)
 	def __str__(self):
 		return self.title
-	def __str__(self):
-		return self.content
-	def __str__(self):
-		return self.picture
-	def __str__(self):
-		return self.cat
-	def __str__(self):
-		return self.user
-	def __str__(self):
-		return self.created_at
+
 
 class Comments(models.Model):
-	content=models.CharField(max_length = 200)
+	text=models.CharField(max_length = 200)
 	user = models.ForeignKey(User)
 	post=models.ForeignKey(Posts)
 	def __str__(self):
 		return self.content
-	def __str__(self):
-		return self.user
-	def __str__(self):
-		return self.post
 
-class Replay(models.Model):
-	content = models.CharField(max_length=200)
+
+class Replies(models.Model):
+	text = models.CharField(max_length=200)
 	user = models.ForeignKey(User)
-	#post = models.ForeignKey(Posts)
 	comment=models.ForeignKey(Comments)
 	def __str__(self):
-		return self.content
-	def __str__(self):
-		return self.user
-	def __str__(self):
-		return self.comment
+		return self.text
+
 
 class Likes(models.Model):
 	type=models.IntegerField()
 	post = models.ForeignKey(Posts)
-	def is_like(self):
-		countlike=0
-		countdislike=0
-		if (self.type == 1):
-			countlike+=1
-		elif(self.type == 0):
-			countdislike += 1
 	def __str__(self):
 		return self.type
-	def __str__(self):
-		return self.post
+
 
 class Tags(models.Model):
 	tag_name=models.CharField(max_length = 15)
 	post = models.ForeignKey(Posts)
 	def __str__(self):
 		return self.tag_name
-	def __str__(self):
-		return self.post
+
 
 class ForbiddenWords(models.Model):
 	word=models.CharField(max_length = 15)
