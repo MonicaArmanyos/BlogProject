fd3f8ec3ae3e227dab9c0f29495b960f6900cf92
nadadahab <nadadahab906@gmail.com>
2018-02-14 11:52:24 +0200

    homepage

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from . import models
+from models import Categories
 
 def homepage(request):
-    context = {'homepage': models.Categories.objects.all()}
+    context = {'homepage': Categories.objects.all()}
     return render(request, 'homepage/homepage.html', context)
+
+def allCategories(request):
+    context={"allCategories":Categories.objects.all()}
+    return render(request,"homepage/homepage.html",context)
