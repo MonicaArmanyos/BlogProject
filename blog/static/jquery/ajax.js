6618080a6cf276a4db35f8c46a58c0d821267a15
diaabrody <diaa_osama70@outlook.com>
2018-02-16 13:37:27 -0500

    ajaxlike

--- blog/static/jquery/ajax.js
+++ blog/static/jquery/ajax.js
@@ -2,13 +2,12 @@ $(document).ready(function()
 {
 
 $("#like").on("click",function(){
-
+var postid=$(this).attr("data-post")
 
   $.ajax({
-       url:"http://127.0.0.1:8000/blog/like",
-
+       url:"http://127.0.0.1:8000/blog/like/"+postid,
        success:function (data) {
-    alert("any")
+    alert(postid)
        }
 
 
